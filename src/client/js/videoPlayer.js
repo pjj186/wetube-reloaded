ad8acdec38ae99d2a61862dca22183a1cb94e3c8
Jong jae Park <pjj186@naver.com>
2021-07-13 17:51:41 +0900

    Timeline

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,6 +4,7 @@ const muteBtn = document.getElementById("mute");
 const volumeRange = document.getElementById("volume");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
 
 let volumeValue = 0.5; // global variable
 video.volume = volumeValue;
@@ -43,10 +44,19 @@ const formatTime = (seconds) => new Date(seconds * 1000).toISOString().substr(11
 
 const handleLoadedMetadata = () => {
     totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = Math.floor(video.duration);
 };
 
 const handleTimeUpdate = () => {
     currentTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
+};
+
+const handleTimelineChange = (event) => {
+    const {
+         target : { value }, 
+        } = event;
+        video.currentTime = value;
 };
 
 playBtn.addEventListener("click", handlePlayClick);
@@ -54,3 +64,4 @@ muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimelineChange);
